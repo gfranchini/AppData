9bb7885224427d65b45c96735f61b0cb42761f2b
Giancarlo Franchini <gfranchini88@gmail.com>
2017-08-11 14:20:53 -0400

    updating has_many association to allow for deletion -> foreign key restraint

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :environments
+  has_many :environments, dependent: :destroy
   accepts_nested_attributes_for :environments,
                                 reject_if: lambda { |attrs| attrs['environment'].blank? }
 end
