5c1602c8f7d98f7c12a03a7de67bb56f9e2e0a81
Giancarlo Franchini <gfranchini88@gmail.com>
2017-10-30 16:46:52 -0400

    Added servers index page and updated seeds file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,18 @@
 end
 
 puts "5 projects were created."
+
+5.times do |x|
+  Server.create!(
+    hostname: "APPDEV#{x+1}",
+    ip: "10.1.10.#{x+1}",
+    cpu: "4",
+    memory: "8",
+    storage: "50",
+    location: "VLAN",
+    operating_system: "Centos7",
+    project_id: "#{rand(1..5)}"
+  )
+end
+
+puts "5 servers were created."
