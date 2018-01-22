8e4a96043456c6c7a440922e2e9e10aaebbe04aa
Giancarlo Franchini <gfranchini@GCs-MacBook-Pro.local>
2018-01-22 17:24:41 -0500

    Modified seeds file with real applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,32 +15,50 @@
 #   )
 # end
 
-Project.create!(
-  name: "Chat",
-  description: "Internal tool to communicate with team members.",
-)
+projects = {
+  "OmegaXL" => "Omega 3 site with Larry King",
+  "GreatHealthWorks": "The GHW store.",
+  "Towzing Site": "Towzing wordpress landing page.",
+  "Chat": "Internal chat tool for IBOs.",
+  "IBO-IMS": "Tool used to check IBO sales.",
+  "Client Services": "Client dashboard to view phone metrics.",
+  "Case": "Used by QA team.",
+  "New You": "Wordpress site for newyou.com",
+  "Great LearningWorks": "Wordpress site.",
+  "NCTD": "Moodle site.",
+  "Prebiophage": "Probiotic site.",
+  "GHW University": "Freddie's moodle site.",
+  "Great VirtualWorks": "Wordpress landing page.",
+  "Pennwell": "Freddie's client's moodle site.",
+  "Click Safety": "Freddie's client's moodle site.",
+  "Great DataWorks": "",
+  "Git GHW": "GHW Applications team internal git repository.",
+  "Testimonials": "Site to collect product testimonials.",
+  "Omaprem": "",
+  "OmegaXLSport": "Landing page for future site.",
+  "Great CloudWorks": "Wordpress site for GCW.",
+  "ProbioticXL": "Probiotic site.",
+  "Miles Dupree": "Miles' bio page.",
+  "Ken Meares": "Ken's bio page.",
+  "Dr. Sharon McQuillan": "The Doc's bio page.",
+  "XL VirtualClassroom": "",
+  "GLW OnDemand": "Moodle store for online learning.",
+  "Try OmegaXLNow": "Marketing site for OmegaXL",
+  "Password Reset": "To reset your password.",
+  "Totalee": "Hair product store.",
+  "WWE OmegaXL": "OmegaXL with WWE sponsorship.",
+  "Towzing App": "Mobile app to summon towers, like Uber."
+}
 
-Project.create!(
-  name: "IBO-IMS",
-  description: "Internal tool to check sales of IBOs.",
-)
+projects.each do |name,description|
+  Project.create!(
+    name: "#{name}",
+    description: "#{description}"
+  )
 
-Project.create!(
-  name: "Client Services",
-  description: "Internal tool to communicate with team members.",
-)
+end
 
-Project.create!(
-  name: "CASE",
-  description: "Internal tool that's always broken.",
-)
-
-Project.create!(
-  name: "Great DataWorks",
-  description: "Database for data.",
-)
-
-puts "5 projects were created."
+puts "#{projects.length} projects were created."
 
 Environment.create!(
   name: "Development",
